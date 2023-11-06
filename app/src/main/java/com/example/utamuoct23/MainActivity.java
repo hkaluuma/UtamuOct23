9253b77bcfd99bb46551f69efe92a826e275299f
hkaluuma <hkaluuma@idi.co.ug>
2023-11-06 18:05:30 +0300

    My first commit

---
+++ app/src/main/java/com/example/utamuoct23/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.utamuoct23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //our variables
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //new logic
+}
