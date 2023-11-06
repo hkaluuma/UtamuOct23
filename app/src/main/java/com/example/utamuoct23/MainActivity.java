257d29f25d5c4c84fb7ccac1fd9414cf008b0e36
hkaluuma <hkaluuma@idi.co.ug>
2023-11-06 18:32:30 +0300

    second commit

--- app/src/main/java/com/example/utamuoct23/MainActivity.java
+++ app/src/main/java/com/example/utamuoct23/MainActivity.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-    //our variables
+public class MainActivity<gotSound> extends AppCompatActivity {
+    //our Global variables
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*multiline comment */
+        //instantiate
+        Dog dogobj =  new Dog();
+        String gotSound = dogobj.makeSound();
+        System.out.println("Printed string: " + gotSound);
     }
-    //new logic
 }
