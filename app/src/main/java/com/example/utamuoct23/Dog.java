060ad09901fbbb25d499389e696eda8ab5126571
hkaluuma <hkaluuma@idi.co.ug>
2023-11-20 18:31:23 +0300

    added changes to Dog & Main

--- app/src/main/java/com/example/utamuoct23/Dog.java
+++ app/src/main/java/com/example/utamuoct23/Dog.java
@@ -1,18 +1,14 @@
 package com.example.utamuoct23;
-
 public class Dog {
     //adding attributes
    public String name, breed, sound, size;
    private int walked_distance;
    int calculatedMovement = 34;
    protected Boolean feed = true;
-
-   //methods
     public String makeSound(){
         String madeSound = "woof woof ....";
         return madeSound;
     }
-    //method of calculating dog movement
     private Integer calculateMovement(){
        walked_distance = 10 + calculatedMovement;
        return walked_distance;
@@ -20,5 +16,4 @@ public class Dog {
     protected void moveDog(){
         String dogmove = "Dog is moving";
     }
-
 }
--- app/src/main/java/com/example/utamuoct23/MainActivity.java
+++ app/src/main/java/com/example/utamuoct23/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity<gotSound> extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         /*multiline comment */
         //instantiate
         Dog dogobj =  new Dog();
